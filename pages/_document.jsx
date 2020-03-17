be8e7c7f55c1c920083dcedaf07a4ee6749babef
unix <unix.bio@gmail.com>
2020-03-17 21:51:56 +0800

    fix: missing component styles on server render

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,18 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import BLOG from '../blog.config'
 import { CSSBaseline } from '@zeit-ui/react'
+import flush from 'styled-jsx/server'
 
 class MyDocument extends Document {
   static async getInitialProps (ctx) {
     const initialProps = await Document.getInitialProps(ctx)
     const styles = CSSBaseline.flush()
-
+  
     return {
       ...initialProps,
       styles: (
         <>
           {initialProps.styles}
           {styles}
+          {flush()}
         </>
       )
     }
