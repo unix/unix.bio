06bcc82a2e2bb4dd7ad36af89198340ba1737c12
unix <unix.bio@gmail.com>
2020-03-23 23:26:05 +0800

    build(scripts): keep avatar image to template

--- scripts/remove-all.js
+++ scripts/remove-all.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra')
 const path = require('path')
-const publicDir = path.join(__dirname, '../public/assets')
+const assetsDir = path.join(__dirname, '../public/assets')
 const postsPath = path.join(__dirname, '../pages/posts')
 const fixedPath = path.join(__dirname, '../pages/fixed')
 const postsTemplate = path.join(__dirname, './templates/posts')
@@ -10,7 +10,12 @@ const fixedTemplate = path.join(__dirname, './templates/fixed')
 ;(async () => {
   await fs.remove(postsPath)
   await fs.remove(fixedPath)
-  await fs.remove(publicDir)
   await fs.copy(postsTemplate, postsPath)
   await fs.copy(fixedTemplate, fixedPath)
+  
+  await Promise.all(fs.readdirSync(assetsDir).map(async name => {
+    if (name === 'avatar.png') return
+    const imagePath = path.join(assetsDir, name)
+    await fs.remove(imagePath)
+  }))
 })()
