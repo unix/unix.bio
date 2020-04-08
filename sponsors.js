bbe2628caf5282ab921dfbcbd1c0a05854c4c4dd
unix <unix.bio@gmail.com>
2020-04-08 12:19:54 +0800

    feat: show sponsors

---
+++ sponsors.js
@@ -0,0 +1,21 @@
+const Sponsors = [
+  {
+    name: '*睿',
+    amount: 0.744,
+  },
+  {
+    name: 'Lzumikonata',
+    amount: 5,
+  },
+  {
+    name: '**清',
+    amount: 1,
+    info: '谢谢维特聚聚~~',
+  },
+  {
+    name: '**强',
+    amount: 10,
+  }
+]
+
+export default Sponsors
