2c90d7dc3b669adad9877498a987cd270c1ee201
unix <unix.bio@gmail.com>
2020-04-08 12:27:23 +0800

    build: ignore some files before generate templates

--- scripts/remove-all.js
+++ scripts/remove-all.js
@@ -1,15 +1,38 @@
 const fs = require('fs-extra')
 const path = require('path')
 const assetsDir = path.join(__dirname, '../public/assets')
+const pagesPath = path.join(__dirname, '../pages')
 const postsPath = path.join(__dirname, '../pages/posts')
 const fixedPath = path.join(__dirname, '../pages/fixed')
 const postsTemplate = path.join(__dirname, './templates/posts')
 const fixedTemplate = path.join(__dirname, './templates/fixed')
+const shouldIgnoreFile = [
+  'sponsors.js',
+]
 
+const removeDirsInPages = async files => {
+  return await Promise.all(files.map(async name => {
+    const fullPath = path.join(pagesPath, name)
+    if (!fs.statSync(fullPath).isDirectory()) return
+  
+    await fs.remove(fullPath)
+  }))
+}
+
+const removeFilesInRoot = async files => {
+  const rootPath = path.join(__dirname, '../')
+  
+  return await Promise.all(files.map(async name => {
+    const fullPath = path.join(rootPath, name)
+    await fs.remove(fullPath)
+  }))
+}
 
 ;(async () => {
-  await fs.remove(postsPath)
-  await fs.remove(fixedPath)
+  const files = await fs.readdir(pagesPath)
+  await removeDirsInPages(files)
+  await removeFilesInRoot(shouldIgnoreFile)
+  
   await fs.copy(postsTemplate, postsPath)
   await fs.copy(fixedTemplate, fixedPath)
   
