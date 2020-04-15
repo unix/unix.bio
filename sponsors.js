3fcec5bdb9173605231ee2053d9bb379152ba6d4
unix <unix.bio@gmail.com>
2020-04-15 12:40:42 +0800

    chore: update sponsors

--- sponsors.js
+++ sponsors.js
@@ -1,15 +1,21 @@
 const Sponsors = [
   {
+    name: 'MonkeyLeeT',
+    amount: 5,
+    info: 'GitHub Sponsor',
+  },
+  {
+    name: 'Lzumikonata',
+    amount: 10,
+    info: 'GitHub Sponsor',
+  },
+  {
     name: '*盈',
     amount: 1.43,
   },
   {
     name: '*睿',
     amount: 0.744,
-  },
-  {
-    name: 'Lzumikonata',
-    amount: 5,
   },
   {
     name: '**清',
