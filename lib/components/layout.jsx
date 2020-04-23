0dbb6fe37ad589edc5fc196f8f19384f52b83868
unix <unix.bio@gmail.com>
2020-04-23 15:51:02 +0800

    fix(layout): fix pre-render content

--- lib/components/layout.jsx
+++ lib/components/layout.jsx
@@ -6,22 +6,37 @@ import Title from './title'
 import { Spacer } from '@zeit-ui/react'
 import { Configs } from '../utils'
 
+const LayoutHeader = ({ meta }) => (
+  <Head>
+    {meta.title && <title>{meta.title} - {Configs.title}</title>}
+    {meta.description && <meta name="description" content={meta.description} />}
+    {meta.description && <meta property="og:description" content={meta.description} />}
+    {meta.title && <meta property="og:title" content={meta.title} />}
+    {meta.image && <meta property="og:image" content={meta.image} />}
+    {meta.image && <meta property="twitter:image" content={meta.image} />}
+  </Head>
+)
+
 const Layout = ({ children, meta = {} }) => {
   const [showAfterRender, setShowAfterRender] = useState(false)
   const inDetailPage = useMemo(() => meta && meta.title, [])
   useEffect(() => setShowAfterRender(true), [])
   
-  if (!showAfterRender) return null
+  if (!showAfterRender) return (
+    <div className="article-content">
+      <LayoutHeader meta={meta} />
+      {children}
+      <style jsx>{`
+        .article-content {
+          opacity: 0;
+          display: none;
+        }
+      `}</style>
+    </div>
+  )
   return (
     <section>
-      <Head>
-        {meta.title && <title>{meta.title} - {Configs.title}</title>}
-        {meta.description && <meta name="description" content={meta.description} />}
-        {meta.description && <meta property="og:description" content={meta.description} />}
-        {meta.title && <meta property="og:title" content={meta.title} />}
-        {meta.image && <meta property="og:image" content={meta.image} />}
-        {meta.image && <meta property="twitter:image" content={meta.image} />}
-      </Head>
+      <LayoutHeader meta={meta} />
       <div className="container">
         <Spacer />
         <Profile />
